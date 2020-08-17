773f9b16d78983184356fb8faac3e4aee697a899
Bonifacio de Oliveira <bonifaciodeoliveira@outlook.com>
2020-08-16 21:29:15 -0400

    Adding cleaning data function

--- getting_data.py
+++ getting_data.py
@@ -10,7 +10,11 @@ class Dataset:
     def __init__(self):
         self.original_dataset = self.get_data()
         self.confirmed_cases = self.get_confirmed_cases(self.original_dataset)
-        self.get_cases_infos(self.original_dataset)
+        self.cleaned_data = None
+        #self.get_cases_infos(self.original_dataset)
+        self.attributes = self.get_attributes_names(self.original_dataset)
+        #self.min_max_dates(self.original_dataset)
+        self.clean_data(self.original_dataset)
 
     def get_data(self):
         request = requests.get('https://covid19.manaus.am.gov.br/wp-content/uploads/Manaus.csv', verify=False)
@@ -26,10 +30,35 @@ class Dataset:
         possible_values = list(data['_classificacao'])
         keys = Counter(possible_values).keys()
         values = Counter(possible_values).values()
-        print(keys)
-        print(values)
+        return keys, values
 
-    def get_attributes_names(self, data);
+    def get_attributes_names(self, data):
+        original_attributes = data.columns.tolist()
+        removed_underline = [i[1:] for i in original_attributes]
+        return removed_underline
+
+    def min_max_dates(self, data):
+        # the date is given by dt_notificacao attribute
+        dates = pd.to_datetime(data['_dt_notificacao'])
+        min_date = dates.min()
+        max_date = dates.max()
+        return min_date, max_date
+
+    def clean_data(self, data):
+        cleaned_data = data[['_idade','_faixa etária','_sexo','_bairro','_classificacao','_dt_notificacao']]
+        print(f'Initial number of lines: {cleaned_data.shape[0]}')
+        cleaned_data = cleaned_data.dropna()
+        print(f'After cleaning, number of lines: {cleaned_data.shape[0]}')
+        cleaned_data = cleaned_data.reset_index(drop=True)
+        return cleaned_data
+
+    def count_data(self, data):
         pass
+
+    def m_and_f_confirmed_percentage(self, data):
+        pass
+
+
+
 
 data = Dataset()
