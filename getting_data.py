4742067b43146083dadc7c96aef23b422099d508
Bonifacio de Oliveira <bonifaciodeoliveira@outlook.com>
2020-08-16 21:30:33 -0400

    most affected sex and recovered percentage functions

--- getting_data.py
+++ getting_data.py
@@ -10,11 +10,12 @@ class Dataset:
     def __init__(self):
         self.original_dataset = self.get_data()
         self.confirmed_cases = self.get_confirmed_cases(self.original_dataset)
-        self.cleaned_data = None
-        #self.get_cases_infos(self.original_dataset)
-        self.attributes = self.get_attributes_names(self.original_dataset)
+        print(self.get_cases_infos(self.original_dataset))
+        #self.attributes = self.get_attributes_names(self.original_dataset)
         #self.min_max_dates(self.original_dataset)
-        self.clean_data(self.original_dataset)
+        self.cleaned_data, self.after_clean_number = self.clean_data(self.original_dataset)
+        #self.recovered_percentage(self.cleaned_data)
+        self.m_and_f_confirmed_percentage(self.cleaned_data)
 
     def get_data(self):
         request = requests.get('https://covid19.manaus.am.gov.br/wp-content/uploads/Manaus.csv', verify=False)
@@ -45,18 +46,40 @@ class Dataset:
         return min_date, max_date
 
     def clean_data(self, data):
-        cleaned_data = data[['_idade','_faixa etária','_sexo','_bairro','_classificacao','_dt_notificacao']]
-        print(f'Initial number of lines: {cleaned_data.shape[0]}')
+        cleaned_data = data[['_idade','_sexo','_bairro','_classificacao','_conclusao','_dt_notificacao','_teste_pcr','_teste_anticorpo','_teste_antigeno','_teste_igm','_teste_igg']]
+        initial_number = cleaned_data.shape[0]
+        print(f'\nInitial number of lines: {initial_number}')
         cleaned_data = cleaned_data.dropna()
-        print(f'After cleaning, number of lines: {cleaned_data.shape[0]}')
+        after_clean_number = cleaned_data.shape[0]
+        print(f'After cleaning, number of lines: {after_clean_number}')
         cleaned_data = cleaned_data.reset_index(drop=True)
-        return cleaned_data
+        return cleaned_data, after_clean_number
 
-    def count_data(self, data):
-        pass
+    def recovered_percentage(self, data):
+        # Poercentagem de individuos recuperados em relação a todos os casos confirmados
+        possible_values = list(data['_conclusao'])
+        keys = Counter(possible_values).keys()
+        values = Counter(possible_values).values()
+        print(f'\nrecovered percentage keys: {keys}')
+        print(f'recovered percentage values: {values}')
+        values_list = list(values)
+        percentage = (values_list[0]/(sum(values_list)))*100 
+        print(f'recovered percentage: {percentage:.2f}%')
+        return (keys, values, percentage)
 
     def m_and_f_confirmed_percentage(self, data):
-        pass
+        possible_values = list(data['_sexo'])
+        keys = Counter(possible_values).keys()
+        values = Counter(possible_values).values()
+        print(keys)
+        print(values)
+        m_f_keys = list(keys)
+        m_f_values = list(values)
+        bigger_values = max(m_f_values)
+        index = m_f_values.index(bigger_values)
+        most_affected = m_f_keys[index]
+        
+        print(f'Os casos acometeram mais {most_affected}')
 
 
 
